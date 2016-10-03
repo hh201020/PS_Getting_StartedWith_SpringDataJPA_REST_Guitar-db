5acf0c8eadb165fd06a603f3e2c8ae93a35216e6
Jerry Hu <hh201020@gmail.com>
2016-10-02 23:49:17 -0400

    JpaRepository as Proxies: Proxy Pattern

--- src/main/java/com/guitar/db/repository/ManufacturerRepository.java
+++ src/main/java/com/guitar/db/repository/ManufacturerRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.guitar.db.model.Manufacturer;
@@ -15,37 +16,43 @@ public class ManufacturerRepository {
 	@PersistenceContext
 	private EntityManager entityManager;
 
+	@Autowired
+	private ManufacturerJpaRepository manufacturerJpaRepository;
 	/**
 	 * Create
 	 */
 	public Manufacturer create(Manufacturer man) {
-		entityManager.persist(man);
-		entityManager.flush();
-		return man;
+		return manufacturerJpaRepository.saveAndFlush(man);
+//		entityManager.persist(man);
+//		entityManager.flush();
+//		return man;
 	}
 
 	/**
 	 * Update
 	 */
 	public Manufacturer update(Manufacturer man) {
-		man = entityManager.merge(man);
-		entityManager.flush();
-		return man;
+		return manufacturerJpaRepository.saveAndFlush(man);
+//		man = entityManager.merge(man);
+//		entityManager.flush();
+//		return man;
 	}
 
 	/**
 	 * Delete
 	 */
 	public void delete(Manufacturer man) {
-		entityManager.remove(man);
-		entityManager.flush();
+		manufacturerJpaRepository.delete(man);
+//
+//		entityManager.remove(man);
+//		entityManager.flush();
 	}
 
 	/**
 	 * Find
 	 */
 	public Manufacturer find(Long id) {
-		return entityManager.find(Manufacturer.class, id);
+		return manufacturerJpaRepository.findOne(id);
 	}
 
 	/**
