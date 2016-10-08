1eb1c7d1a7f77c22cfd868872afc2d8f8ea41f77
Jerry Hu <hh201020@gmail.com>
2016-10-08 14:23:42 -0400

    IsNull / IsNotNull / NotNull

--- src/test/java/com/guitar/db/ModelTypePersistenceTests.java
+++ src/test/java/com/guitar/db/ModelTypePersistenceTests.java
@@ -1,6 +1,9 @@
 package com.guitar.db;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -49,6 +52,12 @@ public class ModelTypePersistenceTests {
 	public void testJpaFind() throws Exception {
 		ModelType mt = modelTypeJpaRepository.findOne(1L);
 		assertEquals("Dreadnought Acoustic", mt.getName());
+	}
+	
+	@Test
+	public void testJpaForNull() throws Exception {
+		List<ModelType> mt = modelTypeJpaRepository.findByNameIsNull();
+		assertNull(null, mt.get(0).getName());
 	}
 	
 //	@Test
