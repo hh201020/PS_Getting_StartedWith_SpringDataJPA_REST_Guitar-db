3c527f77b97216a4f2d94eaf7d8f589a529ff3a0
Jerry Hu <hh201020@gmail.com>
2016-10-08 11:49:00 -0400

    <= / >= / > / < / = for decimals

--- src/main/java/com/guitar/db/repository/ModelJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ModelJpaRepository.java
@@ -1,9 +1,12 @@
 package com.guitar.db.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.guitar.db.model.Model;
 
 public interface ModelJpaRepository extends JpaRepository<Model, Long> {
-
+	List<Model> findByPriceGreaterThanEqualAndPriceLessThanEqual(BigDecimal lowest, BigDecimal highest);
 }
