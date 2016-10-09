dfca7a9daf304cd20bc3632556cfa50b223ac352
Jerry Hu <hh201020@gmail.com>
2016-10-09 13:45:36 -0400

    The should be Projection Excerpts, the last is Virtual Projections

--- src/main/java/com/guitar/db/repository/ModelJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ModelJpaRepository.java
@@ -8,9 +8,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import com.guitar.db.model.Model;
+import com.guitar.db.model.projections.ModelDetailView;
 
+@Repository
+@RepositoryRestResource(excerptProjection=ModelDetailView.class)
 public interface ModelJpaRepository extends JpaRepository<Model, Long> , ModelJpaRepositoryCustom{
 	List<Model> findByPriceGreaterThanEqualAndPriceLessThanEqual(BigDecimal lowest, BigDecimal highest);
 	
